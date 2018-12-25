ae7bf3488a218181ea20c34d62ec3a3190d8d8be
YunlongYang <yunlong17568@gmail.com>
2018-12-25 18:33:09 +0800

    init

---
+++ android/src/main/java/com/github/yyl/update/flutterupdate/FlutterUpdatePlugin.java
@@ -0,0 +1,25 @@
+package com.github.yyl.update.flutterupdate;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** FlutterUpdatePlugin */
+public class FlutterUpdatePlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_update");
+    channel.setMethodCallHandler(new FlutterUpdatePlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+}
